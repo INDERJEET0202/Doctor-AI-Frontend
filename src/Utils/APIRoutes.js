1a718105c828c88b6ff58fc2b3de9e01bfaddbd6
Indrajit Pal <palindrajit10@gmail.com>
2023-07-16 22:50:22 +0530

    / deleted

--- src/Utils/APIRoutes.js
+++ src/Utils/APIRoutes.js
@@ -1,5 +1,5 @@
 // export const host = "http://localhost:3001";
-export const host = "https://doctorai-392406.uw.r.appspot.com/";
+export const host = "https://doctor-ai-node-backend.vercel.app";
 
 export const userLoginRoute = `${host}/login`;
 export const userSignupRoute = `${host}/signup`;
